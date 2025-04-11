92ac2f49592350cbe0c7c607cd63a466581549ee
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2025-04-10 22:16:27 -0500

    Updated/Moderinized

---
+++ README.md
@@ -0,0 +1,54 @@
+# Portflio-React
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Description
+
+I was task with rebuiliding my challenge portfolio using the react library.
+The following is the user story/acceptance criteria for this project.
+
+User Story
+
+AS AN employer looking for candidates with experience building single-page applications
+I WANT to view a potential employee's deployed React portfolio of work samples
+SO THAT I can assess whether they're a good candidate for an open position
+
+Acceptance Criteria
+
+GIVEN a single-page application portfolio for a web developer
+WHEN I load the portfolio
+THEN I am presented with a page containing a header, a section for content, and a footer
+WHEN I view the header
+THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
+WHEN I view the navigation titles
+THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+WHEN I click on a navigation title
+THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+WHEN I load the portfolio the first time
+THEN the About Me title and section are selected by default
+WHEN I am presented with the About Me section
+THEN I see a recent photo or avatar of the developer and a short bio about them
+WHEN I am presented with the Portfolio section
+THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
+WHEN I am presented with the Contact section
+THEN I see a contact form with fields for a name, an email address, and a message
+WHEN I move my cursor out of one of the form fields without entering text
+THEN I receive a notification that this field is required
+WHEN I enter text into the email address field
+THEN I receive a notification if I have entered an invalid email address
+WHEN I am presented with the Resume section
+THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+WHEN I view the footer
+THEN I am presented with text or icon links to the developer’s GitHub and Link
+
+## Screenshot
+
+![alt text](/screenshots/fullscreen1.png)
+![alt text](/screenshots/fullscreen2.png)
+![alt text](/screenshots/smallestscreen.png)
+![alt text](/screenshots/smallestscreen2.png)
+
+### Deployment
+
+You can see the deployed applicaton at:
+https://raidernationbuilder.github.io/portflio-react/
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import selfie from "../src/selfie.jpg";
+import runbuddy from "../src/02-run-buddy.jpg";
+import homepage from "../src/homepage.png";
+import workItDone from "../src/work-it-done.jpg";
+import stillGood from "../src/stillGood.jpg";
+
+function App() {
+  return (
+    <main className="main">
+
+      <header className="header">
+        <h1>Patrick Sebstead</h1>
+        <nav className="nav">
+          <ul>
+            <li><a href="#bio">Bio</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#projects">Projects</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="bio" id="bio">
+        <picture>
+          <img src={selfie} alt="Professional Selfie" />
+        </picture>
+        <article className="about">
+          <h3>About Me</h3>
+          <p>
+          Web Developer with a diverse skillset encompassing LEAN manufacturing implementation and industrial automation. 
+          Technical abilities include working with HMIs, PLCs, and robotics, alongside proficiency in web development. 
+          Recognized as a proactive, resourceful, and persistent problem-solver. Excellent communicator with effective 
+          client relationship-building skills. Graduate of Trilogy Education Services Web Development through the 
+          University of Wisconsin Madison. 
+          </p>
+          <h3>My Skills</h3>
+          <p>
+            Frontend: HTML5, CSS, JavaScript, ReactJS,  
+            Backend:  MySQL, MongoDB, Express,  Node
+          </p>
+          <address id="contact">
+            <h3>Contact Information</h3>
+            <p>425 E. Cecil St</p>
+            <p>Neenah, WI 54956</p>
+            <p>Email: sebstead@yahoo.com</p>
+            <p>Phone: (920) 410-1803</p>
+          </address>
+        </article>
+      </section>
+
+      <section className="projects" id="projects">
+        <h2>Projects</h2>
+        <div className="project-grid">
+          <a href="https://raidernationbuilder.github.io/run-buddy/" target="_blank" className="project-item">
+            <img src={runbuddy} alt="Run Buddy Website" />
+          </a>
+          <a href="https://raidernationbuilder.github.io/finance-assistant/" target="_blank" className="project-item">
+            <img src={homepage} alt="Finance Assistant Website" />
+          </a>
+          <a href="https://warm-eyrie-07140.herokuapp.com/" target="_blank" className="project-item">
+            <img src={workItDone} alt="Work It Done Website" />
+          </a>
+          <a href="https://pacific-bayou-24755.herokuapp.com/" target="_blank" className="project-item">
+            <img src={stillGood} alt="StillGood Website" />
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
